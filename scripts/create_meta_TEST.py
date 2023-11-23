e29c6c5153792a527235699ae0718592441f8056
Ashley Osborne <57442176+aosborne13@users.noreply.github.com>
2023-11-23 12:54:05 +0000

    Update create_meta_TEST.py

--- scripts/create_meta_TEST.py
+++ scripts/create_meta_TEST.py
@@ -24,16 +24,14 @@ def main():
     for entry in filelist:
         if fnmatch.fnmatch(entry, pattern_fw):
             sampleid = entry.split(pattern_fw[:-8], 1)[0]
-            ipath_fw = os.path.join(pathdir, entry)
-            ipath_rv = os.path.join(pathdir, sampleid + pattern_rv[1:])
-            df = pd.DataFrame({'id': [sampleid], 'ip1': [ipath_fw], 'ip2': [ipath_rv]})
-            meta_df = pd.concat([meta_df, df], ignore_index=True)
-
-    meta_df['id'] = meta_df['id'].str.replace('_R1', '').str.replace('_R2', '')
-    meta_df['id'] = meta_df['id'] + ' ' + meta_df['ip1'].str.replace(pathdir, '') + ' ' + meta_df['ip2'].str.replace(pathdir, '')
-    meta_df.to_csv(odir, sep="\t", header=False, index=False)
-    print("meta file generated at " + odir)
+            ipath_fw = os.path.join(pathdir, sampleid+pattern_fw[1:])
+			ipath_rv = os.path.join(pathdir, sampleid+pattern_rv[1:])
+			df = pd.DataFrame({'id' : [sampleid], 'ip1' : [ipath_fw], 'ip2' : [ipath_rv]})
+			df = df[['id','ip1','ip2']]
+			meta_df = meta_df.append(df, ignore_index=True)
+	meta_df.to_csv(odir, sep = "\t", header=False, index=False)
+	print( "meta file generated at " + odir )
+	return()
 
 if __name__ == "__main__":
-    main()
-
+	main()
