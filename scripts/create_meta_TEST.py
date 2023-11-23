f02ff19931d8cb16e8013b33b97833feccf4573d
Ashley Osborne <57442176+aosborne13@users.noreply.github.com>
2023-11-23 13:03:05 +0000

    Update create_meta_TEST.py

--- scripts/create_meta_TEST.py
+++ scripts/create_meta_TEST.py
@@ -15,7 +15,7 @@ def main():
 
     pathdir = args.path_to_fq
     odir = args.output_file
-    meta_df = pd.DataFrame()
+    meta_df = pd.DataFrame(columns=['id', 'ip1', 'ip2'])
     pattern_fw = args.pattern_fw
     pattern_rv = args.pattern_rv
 
@@ -23,11 +23,11 @@ def main():
 
     for entry in filelist:
         if fnmatch.fnmatch(entry, pattern_fw):
-            sampleid = entry.split(pattern_fw[:-11], 1)[0]
+            sampleid = entry.split(pattern_fw[:-8], 1)[0]
             ipath_fw = os.path.join(pathdir, entry)
             ipath_rv = os.path.join(pathdir, sampleid + pattern_rv[1:])
-            row = {'id': sampleid, 'ip1': ipath_fw, 'ip2': ipath_rv}
-            meta_df = meta_df.append(row, ignore_index=True)
+            row = pd.DataFrame({'id': [sampleid], 'ip1': [ipath_fw], 'ip2': [ipath_rv]})
+            meta_df = pd.concat([meta_df, row], ignore_index=True)
 
     meta_df.to_csv(odir, sep="\t", header=False, index=False)
     print("meta file generated at " + odir)
