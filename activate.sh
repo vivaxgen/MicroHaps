24a418fb7261106d25ab4ecf5b5d9170d5c9ecfc
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2023-12-07 14:35:47 +0930

    add activation script

---
+++ activate.sh
@@ -0,0 +1,16 @@
+# MicroHaps-specific activation script
+
+## find our own path
+_script="$(readlink -f ${BASH_SOURCE[0]})"
+
+## Delete last component from $_script ##
+_mydir="$(dirname $_script)"
+
+export MICROHAPS_BASEDIR=${_mydir}
+
+## set everything else here
+
+## uncomment below if you want to  avoid running "python setup.py install"
+# PATH=${MICROHAPS_BASEDIR}/scripts;${PATH}
+
+
