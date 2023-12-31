34cb7c508d5fbb185ede40ff63bafa243a365cb8
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2023-12-31 20:28:51 +0930

    add support for muhaps_pipeline for variant calling which work on top of ngs-pipeline

--- activate.sh
+++ activate.sh
@@ -9,6 +9,13 @@ _mydir="$(dirname $_script)"
 export MICROHAPS_BASEDIR=${_mydir}
 
 ## set everything else here
+NGS_PIPELINE_BASH_SOURCE=${VVG_BASEDIR}/env/ngs-pipeline/bin/activate.sh
+
+export NGSENV_BASEDIR=${_mydir}
+. ${NGS_PIPELINE_BASH_SOURCE}
+PATH=${NGSENV_BASEDIR}/bin:${PATH}
+PYTHONPATH=${NGSENV_BASEDIR}:${PYTHONPATH}
+export NGS_PIPELINE_CMD_MODS=muhaps_pipeline.cmds:${NGS_PIPELINE_CMD_MODS}
 
 ## uncomment below if you want to  avoid running "python setup.py install"
 PATH=${MICROHAPS_BASEDIR}/scripts:${PATH}
---
+++ muhaps_pipeline/rules/00-readme.txt
@@ -0,0 +1,1 @@
+This directory contains snakefile specific for muhaps_pipeline
