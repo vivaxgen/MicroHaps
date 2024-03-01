c29dec16e80ca36a04231fea4e67bf1e56e12e4a
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-03-01 13:46:43 +0930

    add microhaps_pipeline commands

--- activate.sh
+++ activate.sh
@@ -7,15 +7,11 @@ _script="$(readlink -f ${BASH_SOURCE[0]})"
 _mydir="$(dirname $_script)"
 
 export MICROHAPS_BASEDIR=${_mydir}
-
-## set everything else here
-NGS_PIPELINE_BASH_SOURCE=${VVG_BASEDIR}/env/ngs-pipeline/bin/activate.sh
-
 export NGSENV_BASEDIR=${_mydir}
-. ${NGS_PIPELINE_BASH_SOURCE}
+
 PATH=${NGSENV_BASEDIR}/bin:${PATH}
 PYTHONPATH=${NGSENV_BASEDIR}:${PYTHONPATH}
-export NGS_PIPELINE_CMD_MODS=muhaps_pipeline.cmds:${NGS_PIPELINE_CMD_MODS}
+export NGS_PIPELINE_CMD_MODS=muhaps_pipeline.cmds:microhaps_pipeline.cmds:${NGS_PIPELINE_CMD_MODS}
 
 ## uncomment below if you want to  avoid running "python setup.py install"
 PATH=${MICROHAPS_BASEDIR}/scripts:${PATH}
