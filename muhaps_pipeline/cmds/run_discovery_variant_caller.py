f694142abad1a8df91fb845a48d7cc58c9f7b718
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-03-03 01:51:34 +0930

    add run-discovery-variant-caller command

---
+++ muhaps_pipeline/cmds/run_discovery_variant_caller.py
@@ -0,0 +1,26 @@
+# run_discovery_variant_caller.py - ngs-pipeline command
+# [https://github.com/vivaxgen/ngs-pipeline]
+
+__copyright__ = "(C) 2024, Hidayat Trimarsanto <trimarsanto@gmail.com>"
+__license__ = "MIT"
+
+# to improve the responsiveness during bash autocomplete, do not import heavy
+# modules (such as numpy, pandas, etc) here, but instead import them within the
+# functions that require the respective heavy modules
+
+
+from ngs_pipeline.cmds import run_multistep_variant_caller
+
+
+def init_argparser():
+    return run_multistep_variant_caller.init_argparser()
+
+
+def run_discovery_variant_caller(args):
+    run_multistep_variant_caller.run_multistep_variant_caller(args)
+
+
+def main(args):
+    run_discovery_variant_caller(args)
+
+# EOF
