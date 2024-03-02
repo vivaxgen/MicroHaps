754b37127feee0ed1a6aa60cf44190693bb0ef32
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-03-02 09:57:08 +0930

    update with variables for NGS-Pipeline, terminal prompt and history file

--- activate.sh
+++ activate.sh
@@ -11,9 +11,18 @@ export NGSENV_BASEDIR=${_mydir}
 
 PATH=${NGSENV_BASEDIR}/bin:${PATH}
 PYTHONPATH=${NGSENV_BASEDIR}:${PYTHONPATH}
+
+## variable for vivaxGEN NGS-Pipeline
 export NGS_PIPELINE_CMD_MODS=muhaps_pipeline.cmds:microhaps_pipeline.cmds:${NGS_PIPELINE_CMD_MODS}
+export NGS_PIPELINE_FORCE=1
+export NGS_PIPELINE_NO_CONFIG_CASCADE=1
 
 ## uncomment below if you want to  avoid running "python setup.py install"
 PATH=${MICROHAPS_BASEDIR}/scripts:${PATH}
 
+## set prompt
+PS1=$'(\xc2\xb5haps) [\\u@\\h \\W]$ '
+
+export HISTFILE=${HOME}/.bash_history.muhaps
+history -c; history -r
 
