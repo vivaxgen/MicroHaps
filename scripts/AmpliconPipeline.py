2b6252145b2bb8830e135b53029a20d7cf7f624e
Ashley Osborne <57442176+aosborne13@users.noreply.github.com>
2024-03-11 14:06:10 +0000

    Update AmpliconPipeline.py

--- scripts/AmpliconPipeline.py
+++ scripts/AmpliconPipeline.py
@@ -10,6 +10,7 @@ import subprocess as sp
 #from fastq2matrix import run_cmd
 
 MICROHAPS_BASEDIR = "~/tools/MicroHaps/scripts"
+#MICROHAPS_BASEDIR = os.environ['MICROHAPS_BASEDIR']
 
 def run_cmd(cmd):
     sys.stderr.write("Running command:\n%s\n\n" % cmd)
