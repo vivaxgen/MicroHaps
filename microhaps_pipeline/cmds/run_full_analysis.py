382d2f0c35548d0dc698058d461404c1286725c6
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-12-12 17:46:51 +0930

    add: --skip and --no-skip; blacken reformat

--- microhaps_pipeline/cmds/run_full_analysis.py
+++ microhaps_pipeline/cmds/run_full_analysis.py
@@ -1,4 +1,3 @@
-
 import os
 import pathlib
 from ngs_pipeline import cerr, cexit, get_snakefile_path
@@ -6,23 +5,46 @@ from ngs_pipeline.cmds import run_snakefile
 
 # this is a wrapper to run initialize.smk
 
+
 def init_argparser():
 
-    p = run_snakefile.init_argparser('run microhaps full analysis')
-
+    p = run_snakefile.init_argparser("run microhaps full analysis")
 
     m = p.add_mutually_exclusive_group()
-    m.add_argument('--single', default=False, action='store_true',
-                   help='fastq files are single (non-paired) such as ONT reads')
-    m.add_argument('--paired', default=False, action='store_true',
-                   help='fastq files are paired such as Illumina paired-end reads')
+    m.add_argument(
+        "--single",
+        default=False,
+        action="store_true",
+        help="fastq files are single (non-paired) such as ONT reads",
+    )
+    m.add_argument(
+        "--paired",
+        default=False,
+        action="store_true",
+        help="fastq files are paired such as Illumina paired-end reads",
+    )
 
-    p.add_argument('-u', '--underscore', default=0, type=int,
-                   help='number of undercore character to be stripped, counted in reverse')
-    
-    p.add_argument('-o', '--outdir',
-                   help='outdir')
-    p.add_argument('infiles', nargs='+')
+    p.add_argument(
+        "-u",
+        "--underscore",
+        default=0,
+        type=int,
+        help="number of undercore character to be stripped, counted in reverse",
+    )
+
+    p.add_argument(
+        "--skip",
+        default=["Undetermined"],
+        action="append",
+        help="skip samples with the given name (can be used multiple times)",
+    )
+
+    p.add_argument(
+        "--no-skip", default=False, action="store_true", help="do not skip any samples"
+    )
+
+    p.add_argument("-o", "--outdir", help="outdir")
+    p.add_argument("infiles", nargs="+")
     return p
 
 
@@ -30,7 +52,9 @@ def run_full_analysis(args):
 
     import microhaps_pipeline
 
-    args.snakefile = get_snakefile_path('Amplicon_Pipeline.smk', from_module=microhaps_pipeline)
+    args.snakefile = get_snakefile_path(
+        "Amplicon_Pipeline.smk", from_module=microhaps_pipeline
+    )
     args.no_config_cascade = True
     args.force = True
 
@@ -41,18 +65,20 @@ def run_full_analysis(args):
         singleton=args.single,
         paired=args.paired,
         underscore=args.underscore,
-        outdir=args.outdir
+        outdir=args.outdir,
     )
 
-    status, elapsed_time = run_snakefile.run_snakefile(args, config=config, show_status=False)
+    status, elapsed_time = run_snakefile.run_snakefile(
+        args, config=config, show_status=False
+    )
 
     if not status:
-        cerr('[WARNING: run-full-analysis did not successfully complete]')
-    cerr(f'[Finish run-full-analysis (time: {elapsed_time})]')
+        cerr("[WARNING: run-full-analysis did not successfully complete]")
+    cerr(f"[Finish run-full-analysis (time: {elapsed_time})]")
+
 
 def main(args):
     run_full_analysis(args)
 
 
 # EOF
-
